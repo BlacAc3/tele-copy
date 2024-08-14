261a3984aee52ea177ebe9ed1819fa97d2019ed2
BlacAc3 <aceezeala@gmail.com>
2024-08-14 14:29:43 +0100

    Completed

--- main.py
+++ main.py
@@ -23,10 +23,16 @@
 from os import getenv, makedirs
 import pickle
 from sys import exit
+from telethon.sessions.sqlite import PeerChannel, PeerChat
+from telethon.tl.custom import dialog
 import threading
+import asyncio
 
 from dotenv import load_dotenv, find_dotenv
-from telegram.client import Telegram
+from telethon.sync import TelegramClient, events
+from telethon.tl.types import InputPeerChat, InputPeerUser, Message
+
+
 
 load_dotenv(find_dotenv())
 
@@ -34,12 +40,20 @@ load_dotenv(find_dotenv())
 # App Configurations #
 ######################
 
-src_chat = getenv("SOURCE") or None
-dst_chat = getenv("DESTINATION") or None
+
+
+phone = getenv("PHONE", None)
+api_id = getenv("API_ID", None)
+api_hash = getenv("API_HASH", None)
+
+message_has_been_sent = asyncio.Event()
+send_message_result = None
+
+
 
 EXCLUDE_THESE_MESSAGE_TYPES = [
-    "messageChatChangePhoto", 
-    "messageChatChangeTitle", 
+    "messageChatChangePhoto",
+    "messageChatChangeTitle",
     "messageBasicGroupChatCreate",
     "messageChatDeleteMember",
     "messageChatAddMembers",
@@ -49,144 +63,209 @@ EXCLUDE_THESE_MESSAGE_TYPES = [
 # Telegram Configurations #
 ###########################
 
-tg = Telegram(
-    api_id=getenv("API_ID"),
-    api_hash=getenv("API_HASH"),
+# tg = Telegram(
+#     api_id=getenv("API_ID", None),
+#     api_hash=getenv("API_HASH", None),
 
-    phone=getenv("PHONE"),
+#     phone=getenv("PHONE", None),
 
-    database_encryption_key=getenv("DB_PASSWORD"),
-    files_directory=getenv("FILES_DIRECTORY"),
+#     database_encryption_key=getenv("DB_PASSWORD"),
+#     files_directory=getenv("FILES_DIRECTORY", None),
 
-    proxy_server=getenv("PROXY_SERVER"),
-    proxy_port=getenv("PROXY_PORT"),
-    proxy_type={
-          # 'proxyTypeSocks5', 'proxyTypeMtproto', 'proxyTypeHttp'
-          '@type': getenv("PROXY_TYPE"),
-    },
-)
+#     # proxy_server=getenv("PROXY_SERVER"),
+#     # proxy_port=getenv("PROXY_PORT"),
+#     # proxy_type={
+#     #       # 'proxyTypeSocks5', 'proxyTypeMtproto', 'proxyTypeHttp'
+#     #       '@type': getenv("PROXY_TYPE"),
+#     # },
+# )
+
+
 
 ###############
 # App methods #
 ###############
 
-def copy_message(from_chat_id: int, to_chat_id: int, message_id: int, send_copy: bool = True) -> None:
-    data = {
-        'chat_id': to_chat_id,
-        'from_chat_id': from_chat_id,
-        'message_ids': [message_id],
-        'send_copy': send_copy,
-    }
-    result = tg.call_method(method_name='forwardMessages', params=data, block=True)
-    if result.update["messages"] == [None]:
-        raise Exception(f"Message {message_id} could not be copied")
-    return result
+
+async def copy_message(tg: TelegramClient, chat_recipient: int, message_obj: Message):
+    sent_message_obj = await tg.send_message(
+        chat_recipient,
+        message = message_obj.message,
+        reply_to=message_obj.reply_to,
+        silent=message_obj.silent,
+        file=message_obj.media,  # Assuming 'media' is the media file to send
+        grouped_id=message_obj.grouped_id,
+
+        # parse_mode=message_obj.parse_mode,
+        # reply_markup=message_obj.reply_markup,
+        # clear_draft=message_obj.clear_draft,
+        buttons=message_obj.buttons,  # Assuming 'buttons' are the inline keyboard buttons
+    )
+    print("Message sent successfully!\n")
+    return sent_message_obj
 
 
-if __name__ == "__main__":
+
+
+
+
+async def main():
+    message_copy_dict: dict = None
+    src_chat = getenv("SOURCE", None)
+    dst_chat = getenv("DESTINATION", None)
+
+
     try:
         with open("data/message_copy_dict.pickle", "rb") as f:
             message_copy_dict = pickle.load(f)
     except OSError:
         message_copy_dict = dict()
 
-    tg.login()
+    async with TelegramClient(phone, api_id, api_hash) as tg :
 
-    result = tg.get_chats()
-    result.wait()
-    chats = result.update['chat_ids']
-    for chat_id in chats:
-        r = tg.get_chat(chat_id)
-        r.wait()
-        title = r.update['title']
-        print(f"{chat_id}, {title}")
+        #Start Client
+        await tg.start(phone)
 
-    if (src_chat is None or dst_chat is None):
-        print("\nPlease enter SOURCE and DESTINATION in .env file")
-        exit(1)
-    else:
-        src_chat = int(src_chat)
-        dst_chat = int(dst_chat)
+        #get dialogs(chats)
+        dialogs = await tg.get_dialogs()
 
-    print(f"Fetching messages from src_chat {src_chat}...")
-    collector_for_all_message_ids_in_src_chat = []
-    last = 0
-    while True:
-        print(".",end="",flush=True)
-        r = tg.get_chat_history(src_chat, limit=10, from_message_id=last)
-        r.wait()
-        if r.update["total_count"] == 0:
-            break
-        for m in r.update["messages"]:
-            if m["content"]["@type"] in EXCLUDE_THESE_MESSAGE_TYPES:
-                continue
-            collector_for_all_message_ids_in_src_chat.append(m["id"])
-        last = r.update["messages"][-1]["id"]
-    print("\n"+f"Got a total of {len(collector_for_all_message_ids_in_src_chat)} messages")
-    
-    print(f"Fetching messages from dst_chat {dst_chat}...")
-    collector_for_all_message_ids_in_dst_chat = []
-    last = 0
-    while True:
-        print(".",end="",flush=True)
-        r = tg.get_chat_history(dst_chat, limit=10, from_message_id=last)
-        r.wait()
-        if r.update["total_count"] == 0:
-            break
-        for m in r.update["messages"]:
-            collector_for_all_message_ids_in_dst_chat.append(m["id"])
-        last = r.update["messages"][-1]["id"]
-    print("\n"+f"Got a total of {len(collector_for_all_message_ids_in_dst_chat)} messages")
+        # Print information about each chat
+        for dialog in dialogs:
+            print(f'Chat ID: {dialog.id}, Chat Name: {dialog.name}')
 
-    print("Processing...")
-    for message_id in reversed(collector_for_all_message_ids_in_src_chat):
-        #print(".",end="",flush=True)
-        print(message_id)
-        if message_id in message_copy_dict:
-            print(f"found: {message_id} => {message_copy_dict[message_id]}")
-            #print(collector_for_all_message_ids_in_dst_chat)
-            if not message_copy_dict[message_id] in collector_for_all_message_ids_in_dst_chat:
-                print(f"{message_copy_dict[message_id]} not in collector_for_all_message_ids_in_dst_chat")
+        chat_ids = [dialog.id for dialog in dialogs]
+
+        ## Iterate through chats to find specific chats
+        # for chat_id in chat_ids:
+        #     first_channel_found = False
+        #     second_channel_found= False
+        #     r = await tg.get_entity(chat_id)
+
+        #     #handle entities type channel or group or chat
+        #     try:
+        #         title = r.first_name
+        #     except:
+        #         title = r.title
+
+        #     if title == "Test1 Channel":
+        #         first_channel_found = True
+
+        #     if title == "Test2 Channel":
+        #         second_channel_found = True
+        #     if first_channel_found and second_channel_found:
+        #         print(f"{chat_id}, {title}", flush=True)
+        #         break
+        #     else:
+        #         print("Channels not found!!")
+
+
+
+
+        if (not src_chat or not dst_chat):
+            print("\nPlease enter SOURCE and DESTINATION in .env file")
+            exit(1)
+        else:
+            src_chat = int(src_chat)
+            dst_chat = int(dst_chat)
+
+        src_entity =await tg.get_entity(src_chat)
+        dst_entity =await tg.get_entity(dst_chat)
+        print(f'Retrieved Entities: \nSource-->{src_entity.title} \n Destination --> {dst_entity.title}\n')
+
+        print(f"Fetching messages from src_chat {src_entity.title}...")
+        collector_for_all_message_ids_in_src_chat = []
+        last = 0
+        while True:
+            print(".",end="",flush=True)
+            messages = await tg.get_messages(src_chat, limit=10, min_id=last)
+            if not messages.total:
+                break
+            for m in messages:
+                collector_for_all_message_ids_in_src_chat.append(m.id)
+            last = messages[-1].id
+            last_mes = messages[-1]
+
+            print(f"last message:{last} ::: {last_mes.message}")
+            print(f'list of messages: {collector_for_all_message_ids_in_src_chat}')
+
+            if last != 0:
+                break
+
+        print(f"Got a total of {len(collector_for_all_message_ids_in_src_chat)} messages")
+        print()
+
+        print(f"Fetching messages from dst_chat {dst_entity.title}...")
+        collector_for_all_message_ids_in_dst_chat = []
+        last = 0
+        while True:
+            print(".",end="",flush=True)
+            messages = await tg.get_messages(dst_chat, limit=10, min_id=last)
+            if not messages.total:
+                break
+            for m in messages:
+                collector_for_all_message_ids_in_dst_chat.append(m.id)
+            last = messages[-1].id
+            last_mes = messages[-1]
+
+            print(f"last message id:{last}:::: {last_mes.message} ")
+
+            if last != 0:
+                break
+
+        print("Processing...")
+        for message_id in reversed(collector_for_all_message_ids_in_src_chat): #the last message or first message when reversed prints out channel name!
+            #print(".",end="",flush=True)
+            # if message_id in message_copy_dict:
+            #     print(f"found: {message_id} in dict {message_copy_dict[message_id]}")
+            #     #print(collector_for_all_message_ids_in_dst_chat)
+            #     if not message_copy_dict[message_id] in collector_for_all_message_ids_in_dst_chat:
+            #         print(f"{message_copy_dict[message_id]} not in collector_for_all_message_ids_in_dst_chat")
+            #     continue
+            try:
+                print('copyin message')
+                # peer =await tg.get_entity(src_chat)
+                message = await tg.get_messages(src_chat, ids=message_id)
+
+                # # Check if the message is of type Message and not MessageService
+                if not isinstance(message, Message):
+                    print("Fetched message is a <MessageService> Object instead of a <Message> object .")
+                    print(f"\n {message} \n sourceID {src_chat}")
+                    continue
+
+                send_message_result = await copy_message(tg, dst_entity, message)
+            except Exception as e:
+                print(message)
+                print (f"Failed to a send message: {e}")
+                return None
+
+            print(f"sent message:{send_message_result} ")
             continue
-        try:
-            send_message_result = copy_message(src_chat, dst_chat, message_id)
-        except Exception as e:
-            print(e)
-            print("This message could not be copied:")
-            r = tg.get_message(src_chat, message_id)
-            r.wait()
-            print(r.update)
-            continue
-        
-        message_has_been_sent = threading.Event()
-        # The handler is called when the tdlib sends updateMessageSendSucceeded event
-        def update_message_send_succeeded_handler(update):
-            #print(f'Received updateMessageSendSucceeded: {update}')
-            # When we sent the message, it got a temporary id. The server assigns permanent id to the message
-            # when it receives it, and tdlib sends the updateMessageSendSucceeded event with the new id.
-            #
-            # Check that this event is for the message we sent.
-            if update['old_message_id'] == send_message_result.update["messages"][0]['id']:
-                message_id = update['message']['id']
-                message_has_been_sent.set()
-        # When the event is received, the handler is called.
-        tg.add_update_handler('updateMessageSendSucceeded', update_message_send_succeeded_handler)
-        # Wait for the message to be sent
-        message_has_been_sent.wait(timeout=60)
-        print(f'Message has been sent.')
-        tg.remove_update_handler('updateMessageSendSucceeded', update_message_send_succeeded_handler)
-        
-        r = tg.get_chat_history(dst_chat, limit=1)
-        r.wait()
-        new_message_id = r.update["messages"][0]["id"]
+
+
+
+
+
+        r = await tg.get_messages(dst_chat, limit=10)
+        new_message_id = r[0].id
         print(f"created link: {message_id} => {new_message_id}")
         message_copy_dict.update({message_id: new_message_id})
-        
 
-    print("...done.")
 
-    makedirs("data",exist_ok=True)
-    with open("data/message_copy_dict.pickle", "wb") as f:
-        pickle.dump(message_copy_dict, f)
+
+        print("...done.")
+
+        makedirs("data",exist_ok=True)
+        with open("data/message_copy_dict.pickle", "wb") as f:
+            pickle.dump(message_copy_dict, f)
 
     #tg.idle()
+
+
+
+
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    # asyncio.run(main())
