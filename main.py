6e00b4501fb3cc88aed8f05ec7cd8cc80ab65029
BlacAc3 <aceezeala@gmail.com>
2024-08-15 12:45:58 +0100

    cleanly coded

--- main.py
+++ main.py
@@ -30,35 +30,33 @@ import asyncio
 
 from dotenv import load_dotenv, find_dotenv
 from telethon.sync import TelegramClient, events
-from telethon.tl.types import InputPeerChat, InputPeerUser, Message, MessageReplyHeader
+from telethon.tl.types import InputPeerChat, InputPeerUser, Message, MessageReplyHeader, MessageMediaDocument
 
 
 
-# App Configurations #
+# Environment Variables Configuration #
 #---------------------------------------------------------------------------------
 load_dotenv(find_dotenv())
 phone = getenv("PHONE", None)
 api_id = getenv("API_ID", None)
 api_hash = getenv("API_HASH", None)
-
-message_copy_dict: dict = None
+#Source and destination config
 SOURCE_ID = getenv("SOURCE", None)
 DEST_ID = getenv("DESTINATION", None)
 
-message_has_been_sent = asyncio.Event()
+# Literals and Global Configuration
+link_copied_message_id = {}
+message_copy_dict: dict = None
 ALLOWED_MESSAGE_TYPES= [
     MessageReplyHeader,
     Message
 ]
-send_message_result = None
-link_copied_message_id = {}
 
 
 
 
-# App methods #
+# Functions #
 #--------------------------------------------------------------------------
-
 
 #Main function
 async def main():
@@ -66,24 +64,26 @@ async def main():
     src_chat_id = SOURCE_ID
     dst_chat_id = DEST_ID
 
-    try:
-        with open("data/message_copy_dict.pickle", "rb") as f:
-            message_copy_dict = pickle.load(f)
-    except OSError:
-        message_copy_dict = dict()
+    ## Keeping track of previous or already sent messages
+    # try:
+    #     with open("data/message_copy_dict.pickle", "rb") as f:
+    #         message_copy_dict = pickle.load(f)
+    # except OSError:
+    #     message_copy_dict = dict()
 
     async with TelegramClient(phone, api_id, api_hash) as tg :
 
         #Start Client
         await tg.start(phone)
 
-        #get dialogs(chats)
+        #get chats
         dialogs = await tg.get_dialogs()
 
         # Print information about each chat
         for dialog in dialogs:
             print(f'Chat ID: {dialog.id}, Chat Name: {dialog.name}')
 
+        # Gather chat IDs
         chat_ids = [dialog.id for dialog in dialogs]
 
         if (not src_chat_id or not dst_chat_id):
@@ -93,13 +93,14 @@ async def main():
             src_chat_id = int(src_chat_id)
             dst_chat_id = int(dst_chat_id)
 
+        #Get Source and Destination Chat entity
         src_entity =await tg.get_entity(src_chat_id)
         dst_entity =await tg.get_entity(dst_chat_id)
         print(f'Retrieved Entities: \nSource-->{src_entity.title} \n Destination --> {dst_entity.title}\n')
 
         #Fetch ids from source and Destination
         print(f"Fetching messages from src_chat {src_entity.title}...")
-        collector_for_all_message_ids_in_src_chat = await collect_messages(tg,src_chat_id)
+        collector_for_all_message_ids_in_src_chat = await collect_messages(tg, src_chat_id)
         print(f"Got a total of {len(collector_for_all_message_ids_in_src_chat)} messages from source chat")
 
         print()
@@ -108,113 +109,126 @@ async def main():
 
 
         print("Processing...")
+        # Holds previous message after a new iteration starts
+        # and the iteration does not fit in a group of media or is and new group of media.
+        # sendin
+        previous_message: Message = None #holds info about previous iteration to
         grouped_media_caption: str=None
-        grouped_messages=[]
-        group_id: int = None
+        grouped_media: list=[]
+        group_id: int = None  #Identifies the group id of a media message (2 messages with identical group ids are in the same group)
+
+        # Iterates through collected messages in source chat
         for message_id in reversed(collector_for_all_message_ids_in_src_chat): #the last message or first message when reversed prints out channel name!
-            # try:
             message = await tg.get_messages(src_chat_id, ids=message_id)
 
             # Check if the message is of type Message and not MessageService
-            # if not isinstance(message, Message):
-            if type(message) not in  ALLOWED_MESSAGE_TYPES:
-                print(f"Fetched message is a {type(message)} instead of one of these: \n{ALLOWED_MESSAGE_TYPES}")
-                print(f"\n {message} \n sourceID {src_chat_id}")
+            if type(message) not in ALLOWED_MESSAGE_TYPES:
+                print()
+                print(f"Error: Fetched message is a {type(message)} instead of one of these: \n{ALLOWED_MESSAGE_TYPES}")
+                print()
+                print(f"message raising error: --> \n{message}")
+                print()
                 continue
 
-                if isinstance(message, MessageReplyHeader):
-                    print(message)
 
-            #send message if not part of a grouped media
+            # Store to previous message and add caption if message is a MEDIA with a CAPTION
             if message.message != "" and message.media is not None:
                 grouped_media_caption=message.message
+                previous_message = message
 
-            # if text message sent after a grouped media message is created, send media before the text message
-            if message.grouped_id is None:
-                if grouped_messages:
-                    await tg.send_file(dst_chat_id, grouped_messages, caption=grouped_media_caption)
-                    grouped_messages=[]
+            # If new iteration(media message) is not part of a group of media (initiated previously),
+            # after a grouped media has be initiated or created,
+            # Send media(previously set as previous message) before sending current iteration(media)
+            if message.grouped_id is None: # If message is not a grouped message(media)
+                if grouped_media: # If a group of media messages is initiated but not sent/handled
+                    sent_file = await tg.send_file(dst_chat_id, grouped_media, caption=grouped_media_caption)
+                    link_copied_message_id[f"{previous_message.id}"] = sent_file[-1].id
+                    grouped_media=[]
                 # Send a copy of the message
-                send_message_result = await copy_message(tg, dst_entity, message)
+                await copy_message(tg, dst_entity, message)
 
 
 
-            #if message is the first of a group
+            #assign a new group id if group id is empty
             elif group_id is None:
-                grouped_messages.append(message.media)
+                grouped_media.append(message.media)
                 group_id = message.grouped_id
-            #if message is part of an existing group
+                previous_message = message
+            #if message is part of the existing group
             elif message.grouped_id == group_id:
-                grouped_messages.append(message.media)
-            #if message is a start of a new group of media
+                grouped_media.append(message.media)
+                previous_message = message
+            # if message media is not a part of an existing group of media,
+            # Send the existing group of media
+            # create a new group of media.
             elif message.grouped_id != group_id:
                 print("new group for current message found! ")
-                await tg.send_file(dst_chat_id, grouped_messages, caption=grouped_media_caption)
-                # refresh setting for a new group of media
-                grouped_messages = []
+                sent_file=await tg.send_file(dst_chat_id, grouped_media, caption=grouped_media_caption) #send existing group media
+                link_copied_message_id[f"{previous_message.id}"] = sent_file[-1].id
+                # refresh lists for a new group of media and new group id
+                grouped_media = []
                 group_id=[]
-                grouped_messages.append(message.media)
+                # new group configuration
+                grouped_media.append(message.media)
                 group_id = message.grouped_id
             continue
 
-            # except Exception as e:
-            #     print(f"Message causing Error ---> {message}")
-            #     print (f"Error ----> {e}")
-            #     return None
-
-            # print(f"Sending messages...")
-            # continue
-        # if a group of media is still exists but is yet to be sent
-        if grouped_messages:
-            await tg.send_file(dst_chat_id, grouped_messages, caption=grouped_media_caption)
+        # # if a group of media is still exists but is yet to be sent
+        # if grouped_media:
+        #     await tg.send_file(dst_chat_id, grouped_media, caption=grouped_media_caption)
 
 
-        #return info about destination chat
-        r = await tg.get_messages(dst_chat_id, limit=100000)
-        new_message_id = r[0].id
-        print(f"created link: {message_id} => {new_message_id}")
-        message_copy_dict.update({message_id: new_message_id})
+        # Linking source messages and destination messages id to prevent duplication
+        # r = await tg.get_messages(dst_chat_id, limit=100000)
+        # new_message_id = r[0].id
+        # print(f"created link: {message_id} => {new_message_id}")
+        # message_copy_dict.update({message_id: new_message_id})
 
         print("...done.")
-        print(f"Message Links ----> {link_copied_message_id}")
-        makedirs("data",exist_ok=True)
-        with open("data/message_copy_dict.pickle", "wb") as f:
-            pickle.dump(message_copy_dict, f)
+        print(f"Source to Destination Message ID Links ----> {link_copied_message_id}")
+        # makedirs("data",exist_ok=True)
+        # with open("data/message_copy_dict.pickle", "wb") as f:
+        #     pickle.dump(message_copy_dict, f)
 
 
 # get messages from chat id
 # -----------------------------------
 async def collect_messages(tg:TelegramClient, chat_id: int):
-    last = 0
-    message_ids=[]
-    while True:
+    try:
+        message_ids=[]
+        # while True:
         print(".",end="",flush=True)
-        messages = await tg.get_messages(chat_id, limit=100000, min_id=last)
+        messages = await tg.get_messages(chat_id, limit=100000)
         if not messages.total:
-            break
+            print("Chat is Empty!")
+            return message_ids
         for m in messages:
             message_ids.append(m.id)
-        last = messages[-1].id
-        last_mes = messages[-1]
 
-        print(f"last message:{last} ::: {last_mes.message}")
-        print(f'list of messages: {message_ids}')
-
-        if last != 0:
-            break
+        print(f'list of retrieved message IDs: {message_ids}')
+    except Exception as e:
+        print(f"Error Occured in collect_messages function --> {e} ")
+        return None
     return message_ids
 
 
 #Copy function for all message types
 # ----------------------------------------------
 async def copy_message(tg: TelegramClient, chat_recipient: int, message_obj: Message):
-    reply_message = None
+    reply_message:Message = None #Holds message being replied to!
     dest_id = DEST_ID
 
+    # if message is a reply to another message
     if message_obj.reply_to:
-        reply_id_source = message_obj.reply_to.reply_to_msg_id
-        reply_id_dest = link_copied_message_id[f"{reply_id_source}"]
-        reply_message = await tg.get_messages(chat_recipient, ids=reply_id_dest)
+        try:
+            reply_id_source = message_obj.reply_to.reply_to_msg_id
+            reply_id_dest = link_copied_message_id[f"{reply_id_source}"]
+            reply_message = await tg.get_messages(chat_recipient, ids=reply_id_dest)
+        except Exception as e:
+            print()
+            print(f"Error in copy_message function----> {e}")
+            print(message_obj)
+
 
     sent_message_obj = await tg.send_message(
         chat_recipient,
@@ -227,9 +241,10 @@ async def copy_message(tg: TelegramClient, chat_recipient: int, message_obj: Mes
         # reply_markup=message_obj.reply_markup,
         # clear_draft=message_obj.clear_draft,
     )
+
     # append message ID link from old message to new
     link_copied_message_id[f"{message_obj.id}"] = sent_message_obj.id
-    print("Message sent successfully!\n")
+    print(f"Message of ID ->{message_obj.id} sent successfully!")
     return sent_message_obj
 
 
@@ -237,6 +252,6 @@ async def copy_message(tg: TelegramClient, chat_recipient: int, message_obj: Mes
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    # asyncio.run(main())
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
+    asyncio.run(main())
